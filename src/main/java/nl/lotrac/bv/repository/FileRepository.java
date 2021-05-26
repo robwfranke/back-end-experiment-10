b5789630fa4b1340fed74cfd57f91ae9db09e9dd
=Rob Franke <robwfranke@gmail.com>
2021-05-26 14:56:37 +0200

    FileRepository added

---
+++ src/main/java/nl/lotrac/bv/repository/FileRepository.java
@@ -0,0 +1,12 @@
+package nl.lotrac.bv.repository;
+
+import nl.lotrac.bv.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository <Address, Long >{
+
+
+
+
+
+}
